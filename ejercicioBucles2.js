f60c1f11821d87799edbe51b4e95aee0842d0763
Miguel Orts <cesur.s320598@cesurformacion.com>
2023-10-18 09:10:33 +0200

    second commit

---
+++ ejercicioBucles2.js
@@ -0,0 +1,10 @@
+for(let i = 0; i < 50; i++){
+  let potencias = Math.pow(2, i);
+
+  if(potencias > 3000){
+    break;
+  }else{
+    document.write(potencias)
+    document.write("<br>"); 
+  }
+ }
---
+++ tratamientoCadenas.js
@@ -0,0 +1,8 @@
+var nombre = "Miguel";
+var apellido = "Orts";
+var edad = 26;
+var año = 1997;
+alert("Bienvenidos al 'Tratamiento de Cadenas'");
+alert("Mi nombre es " + nombre + "\n" + "y mi apellido es " + apellido);
+alert("La suma de mi edad y de mi año de nacimiento es " + (edad + año));
+alert("Y por último, la suma de todas las variables es " + (nombre+apellido+edad+año));
