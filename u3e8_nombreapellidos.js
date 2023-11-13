f98d9a297b2e10694fc46361a93342c464b020af
MiguelOrtsB <miguelob1997@gmail.com>
2023-11-13 09:10:04 +0100

    .

--- u3e8_nombreapellidos.js
+++ u3e8_nombreapellidos.js
@@ -22,7 +22,25 @@ function mayusculas(nombre){
 function dividirNombre(nombre){
   let array = nombre.split(" ");
   let dividir = document.write("Nombre: " + array[0] + "\n" + "1er Apellido: " + array[1] + "\n" + "2ndo Apellido: " + array[2]);
-  return dividir
+  return dividir;
+}
+
+function nombreUsuario(nombre){
+  let array = nombre.split(" ");
+  let nombre1 = array[0];
+  let apellido1 = array[1];
+  let apellido2 = array[2];
+  let propuestaNombre1 = nombre1.charAt(0) + apellido1 + apellido2.charAt(0);
+  return propuestaNombre1;
+}
+
+function nombreUsuario2(nombre){
+  let array = nombre.split(" ");
+  let nombre1 = array[0];
+  let apellido1 = array[1];
+  let apellido2 = array[2];
+  let propuestaNombre2 = nombre1.substring(0,3) + apellido1.substring(0,3) + apellido2.substring(0,3);
+  return propuestaNombre2;
 }
 
 document.write("Longitud de tu nombre (sin contar espacios): " + longitud(nombre));
@@ -33,3 +51,6 @@ document.write("Nombre en mayúsculas: " + mayusculas(nombre));
 document.write("<br>");
 document.write("Nombre separado en líneas: ") + dividirNombre(nombre);
 document.write("<br>");
+document.write("Propuesta de nombre de usuario: " + nombreUsuario(nombre));
+document.write("<br>");
+document.write("Propuesta de nombre de usuario 2: " + nombreUsuario2(nombre));
